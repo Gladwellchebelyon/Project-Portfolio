144a38415b5b47dfc7885792a8520c842f3fb5b8
Gladwell Chebelyon <hepkorirgladwell@gmail.com>
2025-01-11 11:56:09 +0300

    Initial commit for my Django portfolio project

--- models.py
+++ models.py
@@ -1,52 +1,12 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='project_images/')
-    url = models.URLField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
-
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    link = models.URLField(blank=True)
-
-    def __str__(self):
-        return self.title
-
-# portfolio_app/models.py
-
-from django.db import models
-
-class Project(models.Model):
+class PortfolioItem(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    insights = models.TextField(null=True, blank=True)  # New field for insights
-    demo_url = models.URLField(null=True, blank=True)  # New field for demos
-    github_url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
-
-    def __str__(self):
-        return self.name
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
